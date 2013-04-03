17fb815b06536e6e4d60354561d5d39d5fb3311f
amhokies <amhokies@gmail.com>
2013-04-03 00:37:18 -0400

    Adding Episode 9

---
+++ src/episode_9/Main.java
@@ -0,0 +1,106 @@
+package episode_9;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		/* Random number generator */
+		Random random = new Random();
+		
+		/* Scanner object for input */
+		Scanner scanner = new Scanner(System.in);
+		
+		/*
+		 * Integer variables to hold the user and computer choice.
+		 * 
+		 * 0 = Rock
+		 * 1 = Paper
+		 * 2 = Scissors
+		 * 
+		 */
+		int userChoice, computerChoice;
+		
+		// Showing prompt and user input
+		System.out.println("Enter move (0 = Rock; 1 = Paper; 2 = Scissors):");
+		userChoice = scanner.nextInt();
+		
+		// Checking if userChoice is 0, 1, or 2.
+		if(userChoice < 0 || userChoice > 2)
+		{
+			System.out.println("Invalid choice. Ending program.");
+			
+			// Exit program
+			System.exit(0);
+		}
+		
+		// Generating random computer choice
+		computerChoice = random.nextInt(3);
+		
+		// Determining the winner
+		
+		// If the choices are equal, it's a tie.
+		if(userChoice == computerChoice)
+		{
+			if(userChoice == 0)
+			{
+				System.out.println("Both players chose rock!");
+			}
+			else if (userChoice == 1)
+			{
+				System.out.println("Both players chose paper!");
+			}
+			else
+			{
+				System.out.println("Both players chose scissors!");
+			}
+			
+			// Exit program
+			System.exit(0);
+		}
+		
+		if(userChoice == 0)		// User chooses rock
+		{
+			if(computerChoice == 1)
+			{
+				System.out.println("You chose rock; Computer chose paper");
+				System.out.println("Computer wins!");
+			}
+			else
+			{
+				System.out.println("You chose rock; Computer chose scissors");
+				System.out.println("You win!");
+			}
+		}
+		else if(userChoice == 1)	// User chooses paper
+		{
+			if(computerChoice == 0)
+			{
+				System.out.println("You chose paper; Computer chose rock");
+				System.out.println("You win!");
+			}
+			else
+			{
+				System.out.println("You chose paper; Computer chose scissors");
+				System.out.println("Computer wins!");
+			}
+		}
+		else	// User chooses scissors
+		{
+			if(computerChoice == 0)
+			{
+				System.out.println("You chose scissors; Computer chose rock");
+				System.out.println("Computer wins!");
+			}
+			else
+			{
+				System.out.println("You chose scissors; Computer chose paper");
+				System.out.println("You win!");
+			}
+		}
+		
+		scanner.close();
+	}
+}
